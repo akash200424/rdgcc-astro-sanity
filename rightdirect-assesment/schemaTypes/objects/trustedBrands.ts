d825593714cadf20d2050638fe784d195e537123
akash200424 <akashjayakumar2412@gmail.com>
2026-07-31 16:55:35 +0530

    Simplify trusted brands schema

--- rightdirect-assesment/schemaTypes/objects/trustedBrands.ts
+++ rightdirect-assesment/schemaTypes/objects/trustedBrands.ts
@@ -19,13 +19,8 @@ export default defineType({
           type: 'object',
           fields: [
             defineField({
-              name: 'name',
-              title: 'Brand Name',
-              type: 'string',
-            }),
-            defineField({
               name: 'logo',
-              title: 'Logo',
+              title: 'Brand Logo',
               type: 'image',
               options: {hotspot: true},
             }),
